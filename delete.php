4c1b31feae22ea0b6cac4f5be857ff8e811dbf76
Muhammad Iqbal Gobel <muhammadiqbal.g@yahoo.com>
2023-09-13 20:04:02 +0700

    add file delete.php

---
+++ delete.php
@@ -0,0 +1,11 @@
+<?php
+
+    include_once("connection.php");
+
+    $id = $_GET['id'];
+
+    $delete = mysqli_query($mysqli, "DELETE FROM book WHERE id='$id'");
+
+    header("Location:index.php");
+
+?>
